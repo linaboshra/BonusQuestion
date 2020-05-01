e4aceee5b1892ed8f3e1b2e8c0f5adeba4c496b3
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-05-01 16:40:33 -0400

    Added Question Class

---
+++ src/bonusquestion/Question.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonusquestion;
+
+/**
+ *
+ * @author Lina's PC
+ */
+public class Question {
+    
+}
