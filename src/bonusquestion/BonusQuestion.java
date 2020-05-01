2f286268772b32648447fda0b58ca5eee585c957
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-05-01 16:40:43 -0400

    Added BonusQuestion Class

---
+++ src/bonusquestion/BonusQuestion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonusquestion;
+
+/**
+ *
+ * @author Lina's PC
+ */
+public class BonusQuestion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
